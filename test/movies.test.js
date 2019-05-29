f4ba7d6994a6400ef956a3391bed1bae08e7b799
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-05-29 13:51:03 +0300

    Add supertest and babel-jest.

--- test/movies.test.js
+++ test/movies.test.js
@@ -1,10 +1,16 @@
 import express from "express";
 import MoviesRoute from "../src/movies";
+import request from "supertest";
 
 const app = express();
 const route = MoviesRoute();
 app.use("/movies", route);
 
 describe("Movies route", () => {
-  it("returns movie reviews", () => {});
+  it("returns movie reviews", async () => {
+    const response = await request(app)
+      .get("/movies")
+      .expect(200);
+    expect(response.body.message).toBeDefined();
+  });
 });
